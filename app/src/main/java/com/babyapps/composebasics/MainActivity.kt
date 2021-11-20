a97e71e325a7ca38f9b1a7ece3a03f58f4b91c74
tgmeser <mucteba.eser@outlook.com>
2021-11-20 15:42:58 +0300

    Image Card Usage

--- app/src/main/java/com/babyapps/composebasics/MainActivity.kt
+++ app/src/main/java/com/babyapps/composebasics/MainActivity.kt
@@ -3,63 +3,100 @@ package com.babyapps.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.babyapps.composebasics.ui.theme.ComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier
-                    .background(color = Color.DarkGray)
-                    .fillMaxHeight(0.5f)
-                    .width(300.dp)
-                    .padding(10.dp)
-                    //.padding(top = 20.dp)
-                    // .padding(start = 70.dp)
-                    //.padding(end = 50.dp)
-                    .border(10.dp, Color.Red)
-                    .padding(10.dp)
-                    .border(5.dp, color = Color.Black)
-                    .padding(5.dp)
-                    .border(10.dp, color = Color.Green)
-                    .padding(10.dp)
-                    .border(5.dp, color = Color.Blue)
-                    .padding(5.dp)
-            ) {
-                Text(
-                    text = "Hey",
-                    color = Color.Yellow,
-                    modifier = Modifier.border(5.dp, color = Color.Red)
-                )
-                Spacer(modifier = Modifier.height(50.dp))
-                Text(
-                    text = "There",
-                    color = Color.Cyan,
-                    modifier = Modifier.border(5.dp, color = Color.Magenta).padding(5.dp)
-                )
-                Spacer(modifier = Modifier.height(20.dp))
+            val painter = painterResource(id = R.drawable.recess)
+            val contentDescription = "Recess"
+            val title = "Disney' s Recess"
 
-                Text(text = "People", color = Color.Magenta,modifier=Modifier.clickable {
-                    println("Clicked on text!")
-                })
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(15.dp)
+            ) {
+                ImageCard(painter = painter, contentDescription = contentDescription, title = title)
             }
 
 
         }
     }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(10.dp),// Köşeleri yuvarla
+        elevation = 5.dp  //Gölgeyi ayarla
+    ) {
+        Box(modifier = Modifier.height(300.dp)) {
+            //Sıralama : önce resim, sonra gradient, en son yazı
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+
+            //Gradient
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 400f
+                        )
+                    )
+            )
+
+
+            //Text - Title
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp),
+                contentAlignment = Alignment.BottomStart,
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 15.sp))
+            }
+
+
+        }
+    }
+
 }
 
