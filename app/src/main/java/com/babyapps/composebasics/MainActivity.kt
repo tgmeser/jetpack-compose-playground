d1ff8a9622e243e2bd20aa01befff1fdc0648fba
tgmeser <mucteba.eser@outlook.com>
2021-11-20 14:21:49 +0300

    Modifier usage in detail

--- app/src/main/java/com/babyapps/composebasics/MainActivity.kt
+++ app/src/main/java/com/babyapps/composebasics/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -20,34 +22,41 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
             Column(
                 modifier = Modifier
-                    .fillMaxSize(0.3f).background(color=Color.Yellow),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround
+                    .background(color = Color.DarkGray)
+                    .fillMaxHeight(0.5f)
+                    .width(300.dp)
+                    .padding(10.dp)
+                    //.padding(top = 20.dp)
+                    // .padding(start = 70.dp)
+                    //.padding(end = 50.dp)
+                    .border(10.dp, Color.Red)
+                    .padding(10.dp)
+                    .border(5.dp, color = Color.Black)
+                    .padding(5.dp)
+                    .border(10.dp, color = Color.Green)
+                    .padding(10.dp)
+                    .border(5.dp, color = Color.Blue)
+                    .padding(5.dp)
             ) {
-                Text("Alt")
-                Text("Alta")
-                Text("Koyar")
+                Text(
+                    text = "Hey",
+                    color = Color.Yellow,
+                    modifier = Modifier.border(5.dp, color = Color.Red)
+                )
+                Spacer(modifier = Modifier.height(50.dp))
+                Text(
+                    text = "There",
+                    color = Color.Cyan,
+                    modifier = Modifier.border(5.dp, color = Color.Magenta).padding(5.dp)
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Text(text = "People", color = Color.Magenta,modifier=Modifier.clickable {
+                    println("Clicked on text!")
+                })
             }
-
-
-            /*
-            Row(
-                modifier = Modifier
-                    .fillMaxSize(0.5f)
-                    .background(color = Color.DarkGray),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                Text(text = "Yan", color = Color.White)
-                Text(text = "Yana", color = Color.Cyan)
-                Text(text = "Koyar", color = Color.Yellow)
-
-            }
-
-             */
 
 
         }
