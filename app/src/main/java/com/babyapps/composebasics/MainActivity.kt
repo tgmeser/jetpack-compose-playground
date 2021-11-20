d838ebae1666736009ea065f7f9dfb05cd112e0d
tgmeser <mucteba.eser@outlook.com>
2021-11-20 22:24:46 +0300

    Circular Progress Bar

--- app/src/main/java/com/babyapps/composebasics/MainActivity.kt
+++ app/src/main/java/com/babyapps/composebasics/MainActivity.kt
@@ -20,6 +20,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
@@ -35,6 +37,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ChainStyle
@@ -51,68 +55,62 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            var sizeState by remember {
-                mutableStateOf(200.dp)
-            }
-            //Dp ler büyüyeceği için animateDpAsState seçildi
-            val size by animateDpAsState(
-                targetValue = sizeState,
-                tween(durationMillis = 1000)
-
-
-                /*
-                keyframes {
-                    durationMillis = 5000
-                    sizeState at 0 with LinearEasing
-                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
-                    sizeState * 2f at 5000
-                }
-
-                 */
-
-                /*
-                spring(
-                    Spring.DampingRatioHighBouncy,
-
-                )
-
-                 */
-
-                /*
-                tween(
-                    durationMillis = 3000,
-                    delayMillis = 300,//300 ms sonra harekete geçiyor
-                    easing = LinearOutSlowInEasing
-                )
-
-                 */
-            )
-
-            val infiniteTransition = rememberInfiniteTransition()
-
-            val color by infiniteTransition.animateColor(
-                initialValue = Color.Red,
-                targetValue = Color.Green,
-                animationSpec = infiniteRepeatable(
-                    tween(durationMillis = 2000),repeatMode = RepeatMode.Reverse
-                )
-            )
-
-            Box(
-                modifier = Modifier
-                    .size(size)
-                    .background(color = color),
-                contentAlignment = Alignment.Center
-            ) {
-                Button(onClick = {
-                    sizeState += 50.dp
-                }) {
-                    Text(text = "Increase Size")
-                }
+            Box(contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()){
+                ShowProgressBar(percentage = 0.92f, number = 100)
             }
 
         }
     }
 }
 
+@Composable
+fun ShowProgressBar(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 30.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Blue,
+    strokeWidth: Dp = 10.dp,
+    animationDuration: Int = 1000,
+    animationDelay: Int = 0
+) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val curPer = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        tween(
+            durationMillis = animationDuration,
+            delayMillis = animationDelay
+        )
+    )
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ) {
+
+        //Canvasın içinde olduğu box ile aynı boyutta olmasını istiyoruz bu yüzden yine aynı size verdik
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                -90f,
+                360 * curPer.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        Text(
+            text = (curPer.value * number).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
+    }
+
+}
 
